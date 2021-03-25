2066afd6d7a0ca31ee533ed3f87ac1d53e839831
tugasSBD <76605748+tugasSBD@users.noreply.github.com>
2021-03-26 02:04:58 +0700

    Perbaikan pada card.js

--- src/js/card.js
+++ src/js/card.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/css/card.css'
 import 'bootstrap';
-import '../components/NavBar.js';
-import '../components/CardFeatures.js';
+import '../../components/NavBar.js';
+import '../../components/CardFeatures.js';
