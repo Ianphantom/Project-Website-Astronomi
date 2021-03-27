d45b8104f5ae81f0cd454b1ab516e406eeb20dce
tugasSBD <ian25yola@gmail.com>
2021-03-28 00:50:25 +0700

    Pembuatan apod.js untuk melakukan load page

---
+++ src/js/apod.js
@@ -0,0 +1,28 @@
+import * as api from "./api.js";
+import { saveNasaPictures } from'./api.js';
+window.saveNasaPictures  = saveNasaPictures;
+document.addEventListener("DOMContentLoaded", function() {
+    loadNav();
+   
+    function loadNav() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status != 200) return;
+   
+          // Muat daftar tautan menu
+          document.querySelectorAll(".navigasi").forEach(function(elm) {
+            elm.innerHTML = xhttp.responseText;
+          });
+        }
+      };
+      xhttp.open("GET", "../../pages/apodNav.html", true);
+      xhttp.send();
+    }
+
+    let page = window.location.hash.substr(1);
+    if (page === "" || page == "apodcard"){
+        api.getAPOD();
+    }
+    
+});
