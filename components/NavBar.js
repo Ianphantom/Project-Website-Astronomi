87652100320105f075f1fb17cc98cfd6f8038ebf
tugasSBD <ian25yola@gmail.com>
2021-03-29 14:51:54 +0700

    Perbaikan href pada navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,14 +7,14 @@ class NavBar extends HTMLElement {
         this.innerHTML = `
         <nav class="navbar navbar-expand-lg navbar-light" >
             <div class="container">
-                <a class="navbar-brand" href="#">ASTRONS</a>
+                <a class="navbar-brand" href="index.html">ASTRONS</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav ml-auto">
-                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="#">About</a>
+                    <a class="nav-item nav-link active" href="index.html">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="card.html">Features</a>
                     <a class="nav-item nav-link" href="#">Contact</a>
                     </div>
                 </div>
